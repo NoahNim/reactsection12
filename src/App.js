66bb8ddc9369b515e8120a87433597ad59d21086
noahnim <nmedoff123@gmail.com>
2022-06-20 16:02:14 -0700

    make conrext provider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import AuthContext from "./store/auth-context";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
@@ -30,13 +30,13 @@ function App() {
   };
 
   return (
-    <React.Fragment>
+    <AuthContext.Provider>
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
